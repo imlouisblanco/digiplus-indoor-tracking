8d582c7d4222f0ff1f33ab5f097f6401395cca74
Luis Blanco <luisbastian.blanco@gmail.com>
2025-09-23 18:42:27 -0300

    add battery

--- app/actions/data.js
+++ app/actions/data.js
@@ -3,13 +3,19 @@ import { supabase } from "@/lib/supabase";
 
 export const insertData = async (deviceId, data) => {
   console.log("Inserting data");
+  const deviceEuid = data?.end_device_ids?.dev_eui;
+  const battery = data?.uplink_message?.decoded_payload?.batt_level || null;
+
   const { error } = await supabase.from("data").insert({
     values: data,
-    device_id: deviceId
+    device_id: deviceId,
+    device_euid: deviceEuid,
+    battery: battery
   });
+
   if (error) {
     console.log(error.message);
   }
 
   console.log("Data inserted successfully");
-};
+}
