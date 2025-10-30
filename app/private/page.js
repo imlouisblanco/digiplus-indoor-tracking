a93a1710a53803c8d53a8d7de75c59cdc5ca6188
Luis Blanco <luisbastian.blanco@gmail.com>
2025-10-30 14:16:40 -0300

    fix map

--- app/private/page.js
+++ app/private/page.js
@@ -6,5 +6,6 @@ export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const latestData = await getDevicesLatestData();
+
   return <Dashboard latestData={latestData} />;
 }
